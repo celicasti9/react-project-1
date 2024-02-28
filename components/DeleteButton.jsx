ee58b346b26cfdbc8b2e95c6e2da409af17ef9e8
Celiel Castillo <celiel13@gmail.com>
2024-02-28 17:57:01 -0400

    Starting to work with delete button.

---
+++ components/DeleteButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DeleteButton() {
+    return (
+        <div className="DeleteButton">
+         <div>
+            <button onClick={() => alert("This feature is not yet available.")}> Delete</button>
+         </div>
+        </div>
+    )
+}
+
+export default Footer;
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,69 +2,25 @@ import React from "react";
 
 function Sidebar() {
     return (
-        <nav id="navbar">
-      <ul class="navbar-items flexbox-col">
-        <li class="navbar-logo flexbox-left">
+        <div className="sidebar">
+        <ul>
+            <h3>
+                Sidebar
+            </h3>
+            <li><a href="#">home</a>
 
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="search-outline"></ion-icon>
-            </div>
-            <span class="link-text">Search</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="home-outline"></ion-icon>
-            </div>
-            <span class="link-text">Home</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="folder-open-outline"></ion-icon>
-            </div>
-            <span class="link-text">Projects</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="pie-chart-outline"></ion-icon>
-            </div>
-            <span class="link-text">Dashboard</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="people-outline"></ion-icon>
-            </div>
-            <span class="link-text">Team</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="chatbubbles-outline"></ion-icon>
-            </div>
-            <span class="link-text">Support</span>
-          </a>
-        </li>
-        <li class="navbar-item flexbox-left">
-          <a class="navbar-item-inner flexbox-left">
-            <div class="navbar-item-inner-icon-wrapper flexbox">
-              <ion-icon name="settings-outline"></ion-icon>
-            </div>
-            <span class="link-text">Settings</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+            </li>
+            <li><a href="#"></a>
+
+            </li>
+            <li><a href="#"></a>
+
+            </li>
+            <li><a href="#"></a>
+
+            </li>
+        </ul>
+        </div>
     )
 }
 
