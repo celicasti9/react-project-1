eb9eae1071ba137e955d48989cf4d00af81bcc23
Celiel Castillo <celiel13@gmail.com>
2024-02-28 21:00:56 -0400

    Playing with the list

--- components/List.jsx
+++ components/List.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const listOfItems = ""
+const List = () => {
+  const [jsonData, setJsonData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/src/data/products.json'); // Adjust the path based on your project structure
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.log('Error fetching JSON data:', error);
+      }
+    };
 
-function List () {
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once when the component mounts
+
+  const handleDelete = (itemId) => {
+    // Implement your logic to delete the item from the list
+    // For example, update the state or make an API call
+    console.log(`Deleting item: ${name}`);
+  };
+
   return (
-    <div className='list'>
-      <h2>List of items</h2>
-
-      {}
+    <div>
+      <h2>List of Items</h2>
+      <ul>
+        {jsonData.map((name) => (
+          <List key={name.id} item={name} onDelete={handleDelete} />
+        ))}
+      </ul>
     </div>
   );
-}
-
+};
 
 export default List;
