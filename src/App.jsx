830adb56914ec3e808ba994fb6822fe147fb84fc
Celiel Castillo <celiel13@gmail.com>
2024-03-01 14:27:51 -0400

    Started working the dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,24 @@ import './App.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import AboutPage from '../src/pages/AboutPage'
+import Dashboard from '../src/pages/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
-    
     <div className='App'>
       <Navbar />
-      <Sidebar />
-      <h1>Welcome to the main page baby!</h1>     
-        
+      <Sidebar />       
        <Routes>
         <Route path="/" element={<App />}/>
-        <Route path="/about" element={<AboutPage />}/>
-        
+        <Route path="/about" element={<AboutPage />}/>       
       </Routes>
       </div>
-      <Footer />
-      
+      <Footer />   
     </>
   )
 }
